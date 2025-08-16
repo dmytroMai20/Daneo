d783b84375dccae0829a9a6800d4796c6b24f07c
Dmytro Mai <dm933@cam.ac.uk>
2025-08-16 17:51:49 +0100

    feat: added supabase connection

---
+++ backend/db/db.go
@@ -0,0 +1,26 @@
+package db
+
+import (
+	"context"
+	"log"
+	"os"
+
+	"github.com/jackc/pgx/v5/pgxpool"
+)
+
+var Pool *pgxpool.Pool
+
+func ConnectDB() {
+	dsn := os.Getenv("SUPABASE_DB_URL") // store your URL in env var
+	config, err := pgxpool.ParseConfig(dsn)
+	if err != nil {
+		log.Fatalf("Unable to parse config: %v", err)
+	}
+
+	Pool, err = pgxpool.NewWithConfig(context.Background(), config)
+	if err != nil {
+		log.Fatalf("Unable to connect to database: %v", err)
+	}
+
+	log.Println("Connected to Supabase PostgreSQL with pgx")
+}
