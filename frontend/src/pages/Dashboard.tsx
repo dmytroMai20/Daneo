a99e47640d1815156b71139dcc24ce115a7266b4
Dmytro Mai <dm933@cam.ac.uk>
2025-08-23 20:55:37 +0100

    feat: added shadcn ui integration. Added hompage and login page

---
+++ frontend/src/pages/Dashboard.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Dashboard: React.FC = () => {
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-gray-100">
+      <h1 className="text-4xl font-bold text-gray-800">
+        Welcome to My Website 🚀
+      </h1>
+    </div>
+  );
+};
+
+export default Dashboard;
