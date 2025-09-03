54d473f4c03c9b101a3f0d35f3ec02b1bfa25739
Dmytro Mai <dm933@cam.ac.uk>
2025-09-03 13:43:48 +0100

    fix: removed redundant comments

--- frontend/src/auth/auth.ts
+++ frontend/src/auth/auth.ts
@@ -1,13 +1,10 @@
-// src/auth.ts
 import supabase from "../utils/supabaseClient";
 
-// Sign up with email & password
 export async function signUp(email: string, password: string) {
   const { data, error } = await supabase.auth.signUp({ email, password });
   return { data, error };
 }
 
-// Login with email & password
 export async function signIn(email: string, password: string) {
   const { data, error } = await supabase.auth.signInWithPassword({
     email,
@@ -16,7 +13,6 @@ export async function signIn(email: string, password: string) {
   return { data, error };
 }
 
-// Logout
 export async function signOut() {
   const { error } = await supabase.auth.signOut();
   if (error) throw error;
